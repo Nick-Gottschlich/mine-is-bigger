9095bae22ba5c41830b423ed325f8d74bfd539ee
Nick Gottschlich <nicholas.gottschlich@procore.com>
2019-08-24 17:01:51 -0500

    works but window.onload stuff weird

--- fake-react-app/src/App.js
+++ fake-react-app/src/App.js
@@ -3,10 +3,15 @@ import mineIsBigger from 'mine-is-bigger';
 import './App.css';
 
 function App() {
+
+  const biggestZIndex = mineIsBigger();
+
   return (
     <div>
+      <div>
+      {biggestZIndex}
+      </div>
       <div style={{zIndex: 99}}>
-        {mineIsBigger()}
         This is a fake app to test importing the bundle and detecting z-indexes and stuff
       </div>
       <div className="bigOlZIndex">
--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 "use strict"
 
 module.exports = function mineIsBigger() {
-  console.log('test')
+  window.onload = function () { 
+    let maxZIndex = 0
 
-  console.log(document.documentElement.innerHTML)
+    // get all the html on the page, once everything has loaded
+    const pageSource = document.documentElement.innerHTML
+    // match z-index: 0 or more whitespace, 1 or more integers, capture the integers
+    const matchedZIndices = pageSource.match(/z-index:\s*(\d+)/g);
 
-  // var regexp = /
+    // loop through every z-index value, and keep track of the max
+    for (const item of matchedZIndices) {
+      const zIndexValue = item
+        .split(':')[1]
+        .replace(/\s/g,'')
 
+      if (zIndexValue > maxZIndex) {
+        maxZIndex = zIndexValue;
+      };
+    }
+
+    console.log(maxZIndex)
+    return maxZIndex;
+  }
 }
